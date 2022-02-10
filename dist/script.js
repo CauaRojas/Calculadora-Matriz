0ce42713e18e29c42e156c245eaa3e5af9b93447
cauarojas <cauarojas@hotmail.com>
2022-02-09 23:09:34 -0300

    Adicionei o js basico

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,39 @@
 "use strict";
-var Matriz = /** @class */ (function () {
-    function Matriz(numbers) {
+class Matriz {
+    numbers;
+    constructor(numbers) {
         this.numbers = numbers;
     }
-    return Matriz;
-}());
+}
+const buttonRow = document.querySelector('#buttonRow');
+const buttonCollumn = document.querySelector('#buttonCollumn');
+const leftDiv = document.querySelector('div#left');
+const rightDiv = document.querySelector('div#right');
+const divs = [leftDiv, rightDiv];
+console.log(divs);
+const addCollumn = () => {
+    divs.forEach((div) => {
+        const divChildren = Array.from(document.querySelectorAll('#' + div?.id + ' *'));
+        divChildren.forEach((childDiv) => {
+            const input = document.createElement('input');
+            input.type = 'number';
+            childDiv.appendChild(input);
+        });
+    });
+};
+const addRow = () => {
+    divs.forEach((div) => {
+        const divChildren = Array.from(document.querySelectorAll('#' + div?.id + ' *'));
+        const newRow = document.createElement('div');
+        newRow.className = 'row';
+        for (let i = 1; i <= divChildren[0].childElementCount; i++) {
+            const newInput = document.createElement('input');
+            newInput.type = 'number';
+            newRow.appendChild(newInput);
+        }
+        div?.appendChild(newRow);
+    });
+};
+buttonCollumn.addEventListener('click', addCollumn);
+buttonRow.addEventListener('click', addRow);
+console.log(addCollumn);
--- ts/script.ts
+++ ts/script.ts
@@ -1,3 +1,45 @@
 class Matriz {
 	constructor(public numbers: Array<Array<Number>>) {}
 }
+
+const buttonRow = document.querySelector('#buttonRow') as HTMLButtonElement;
+const buttonCollumn = document.querySelector(
+	'#buttonCollumn'
+) as HTMLButtonElement;
+
+const leftDiv = document.querySelector('div#left');
+const rightDiv = document.querySelector('div#right');
+const divs = [leftDiv, rightDiv];
+console.log(divs);
+
+const addCollumn = () => {
+	divs.forEach((div) => {
+		const divChildren = Array.from(
+			document.querySelectorAll('#' + div?.id + ' *')
+		);
+		divChildren.forEach((childDiv) => {
+			const input = document.createElement('input');
+			input.type = 'number';
+			childDiv.appendChild(input);
+		});
+	});
+};
+const addRow = () => {
+	divs.forEach((div) => {
+		const divChildren = Array.from(
+			document.querySelectorAll('#' + div?.id + ' *')
+		);
+		const newRow = document.createElement('div');
+		newRow.className = 'row';
+		for (let i = 1; i <= divChildren[0].childElementCount; i++) {
+			const newInput = document.createElement('input');
+			newInput.type = 'number';
+			newRow.appendChild(newInput);
+		}
+		div?.appendChild(newRow);
+	});
+};
+
+buttonCollumn.addEventListener('click', addCollumn);
+buttonRow.addEventListener('click', addRow);
+console.log(addCollumn);
