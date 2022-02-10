782836e0f9b30970feff70b3f966f54fe18b9a26
Gustavo de Oliveira <go609127@gmail.com>
2022-02-09 22:53:44 -0300

    Update script.ts

--- ts/script.ts
+++ ts/script.ts
@@ -1,4 +1,26 @@
 class Matriz {
-	constructor(public Numbers: Array<Array<Number>>) {}
+	constructor(public numbers: Array<Array<Number>>) {}
 
+	sub(numbers2: Array<Array<Number>>){
+        if(this.numbers.length !== numbers2.length){
+            console.log("the arrays haven't the same length");
+            return;
+        }
+
+        var i = 0;
+
+        while(this.numbers.length >= i){
+
+			var n = 0;
+			while(this.numbers[n].length >= n){
+
+				numbers2[i];
+				this.numbers[i];
+				n++
+			}
+		
+			i++
+        }
+		
+    }
 }
