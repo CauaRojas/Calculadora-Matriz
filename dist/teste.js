de59cb3a7cd84671d989827dd1ed5442cdb712c3
ReidakaiMFZ <richard_maldito2@hotmail.com>
2022-02-19 15:23:16 -0300

    arquivo de teste

---
+++ dist/teste.js
@@ -0,0 +1,7 @@
+"use strict";
+const matriz = require('./script.js');
+var mat = new matriz([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+var mat2 = new matriz([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+
+console.log(mat.multiplicate(mat2));
+
